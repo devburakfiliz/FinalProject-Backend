9af8da806875c9c3d2ed46a5e91114227578adb6
Burak Filiz <dev.burakfiliz@gmail.com>
2023-03-05 16:06:52 +0300

    added guard

--- Business/ValidationRules/FluentValidation/ProductValidator.cs
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -13,10 +13,10 @@ namespace Business.ValidationRules.FluentValidation
         public ProductValidator()
         {
             RuleFor(p => p.ProductName).NotEmpty();
-            RuleFor(p => p.UnitPrice).NotEmpty();
-            RuleFor(p => p.ProductName).MinimumLength(2);
-            RuleFor(p => p.UnitPrice).GreaterThan(0);
-            RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);
+            //RuleFor(p => p.UnitPrice).NotEmpty();
+            //RuleFor(p => p.ProductName).MinimumLength(2);
+            //RuleFor(p => p.UnitPrice).GreaterThan(0);
+            //RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);
             RuleFor(p => p.ProductName).Must(StartWithA);
         }
 
